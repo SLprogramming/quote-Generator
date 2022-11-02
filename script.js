4ee8565ab139d0c56221b72c66b7096bdf43d651
SL programming:git config --global user.name SL <you@example.com>
2022-11-02 13:53:54 +0630

    quote Generator

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("BMPS javascript developer");
+const newQuote = document.querySelector(".next");
+const texts = document.getElementById("quote");
+const authors = document.getElementById("author");
+const loader = document.getElementById("loader");
+const box = document.querySelector(".box");
+let quotes = [];
+
+const loading = () => {
+  loader.hidden = false;
+  box.hidden = true;
+};
+const dataShow = () => {
+  box.hidden = false;
+  loader.hidden = true;
+};
+const randonNum = () => {
+  let ranNum = Math.floor(Math.random() * quotes.length);
+  texts.textContent = quotes[ranNum]["text"];
+  authors.textContent = quotes[ranNum]["author"];
+  if (!quotes[ranNum].author) {
+    quotes[ranNum].author = "anonymous";
+  }
+  if (quotes[ranNum].text.length > 50) {
+    texts.classList.add("long-quote");
+  } else {
+    texts.classList.remove("long-quote");
+  }
+};
+async function generateQuote() {
+  loading();
+  const linkUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(linkUrl);
+    quotes = await response.json();
+    dataShow();
+    randonNum();
+  } catch (error) {
+    console.log(error);
+  }
+}
+generateQuote();
+newQuote.addEventListener("click", randonNum);
